8c8fd5ffae9e1b7d6845c42f1f97fa025aaa3715
chugogo <ioueasonae86@yahoo.com.tw>
2022-01-12 16:05:32 +0800

    Initial Commit

---
+++ pingpongGoat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pingpongGoat
+//
+//  Created by Chu Go-Go on 2022/1/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
